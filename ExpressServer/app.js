c7d632a2f0f2ad9bd651d17a7efa2277cd2cbf08
ChillyChaos <josh123198@gmail.com>
2023-03-01 01:20:59 -0800

    Added dependencies and plugins. Mainly added connnection to firebase to allow for plugin use.

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -1,10 +1,50 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const admin = require("firebase-admin");
+const router = express.Router();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+const my_databaseURL = "https://<your-firebase-project-id>.firebaseio.com";
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: my_databaseURL,
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+// Create a new user in Firebase Authentication
+router.post("/users", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      data: userRecord.toJSON(),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
+
+// Get a user from Firebase Authentication
+router.get("/users/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userRecord = await admin.auth().getUser(userId);
+
+    res.status(200).json({
+      message: "User retrieved successfully",
+      data: userRecord.toJSON(),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
